ce274ec6397c890ae5ecc82fdc0a9d88cd82f12e
Exstaa <exstagithub@gmail.com>
2024-09-23 22:22:32 +0400

    main class

---
+++ az/hafizrzazade/todolistapi/ToDoListApiMainClass.java
@@ -0,0 +1,11 @@
+package az.hafizrzazade.todolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApiMainClass {
+     public static void main(String[] args) {
+    	 SpringApplication.run(ToDoListApiMainClass.class, args);
+     }
+}
