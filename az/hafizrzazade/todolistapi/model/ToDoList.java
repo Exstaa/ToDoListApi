681a0f68afd937e2cc5acfbf9e44977b424be28a
Exstaa <exstagithub@gmail.com>
2024-09-23 22:26:17 +0400

    todolist model

---
+++ az/hafizrzazade/todolistapi/model/ToDoList.java
@@ -0,0 +1,40 @@
+package az.hafizrzazade.todolistapi.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.springframework.scheduling.config.Task;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ToDoList {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    
+    private String description;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    @OneToMany(mappedBy = "todoList", cascade = CascadeType.ALL)
+    private List<Task> tasks;
+}
